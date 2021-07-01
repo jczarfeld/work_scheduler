aa1c1308b93180a7fbd2b1ef2eef9bb00f889732
John Osterfeld <jczarfeld@gmail.com>
2021-07-01 17:14:49 -0400

    third commit with live time clokc at top and corrected rows with only 9 work hours

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
+var timeDisplayEl = $('#time-display');
+var projectsList = { "9":  [],"10": [],"11": [],"12": [],"13": [],"14": [],"15": [],"16": [],"17": []
+};
+
+
+
+
+
+function displayTime() {
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }
+
+
 $(document).ready(function () {
 
 
 var saveBtn = $(".saveBtn");
 
 saveBtn.on("click", function () {
-    console.log("clicky click")
+  
+    
     var value = $(this).siblings(".description").val()
     var time = $(this).parent().attr("id")
-    console.log(value);
-    console.log(time);
+    localStorage.setItem(value);
+    localStorage.setItem(time);
+})
+
+
+
+
+   
 })
 
 console.log(moment().format(`dddd, MMMM Do`));
 
+setInterval(displayTime, 1000);
 
 
 
@@ -52,4 +74,3 @@ console.log(moment().format(`dddd, MMMM Do`));
 
 
 
-})
